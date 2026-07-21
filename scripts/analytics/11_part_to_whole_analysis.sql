8d1f71db73b0d53149e270fb91ff3187a884e63b
Muhammad Shaaf <155107780+MuhammadShaaf@users.noreply.github.com>
2026-07-21 20:08:00 +0500

    Create 11_part_to_whole_analysis.sql

---
+++ scripts/analytics/11_part_to_whole_analysis.sql
@@ -0,0 +1,50 @@
+/*
+===============================================================================
+Part-to-Whole Analysis
+===============================================================================
+Purpose:
+    - To compare performance or metrics across dimensions or time periods.
+    - To evaluate differences between categories.
+    - Useful for A/B testing or regional comparisons.
+
+SQL Functions Used:
+    - SUM(), AVG(): Aggregates values for comparison.
+    - Window Functions: SUM() OVER() for total calculations.
+===============================================================================
+*/
+-- Which categories contribute the most tp overall sales?
+WITH category_sales AS (
+SELECT 
+category,
+SUM(sales_amount) total_sales
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY category)
+
+SELECT 
+category,
+total_sales,
+SUM(total_sales) OVER() overall_sales,
+CONCAT(ROUND((CAST (total_sales AS FLOAT) / SUM(total_sales) OVER())*100, 2), '%') AS percentage_of_total
+FROM category_sales
+ORDER BY total_sales DESC
+
+
+-- Which sub-categories contribute the most tp overall sales?
+WITH sub_category_sales AS (
+SELECT 
+subcategory,
+SUM(sales_amount) total_sales
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY subcategory)
+
+SELECT 
+subcategory,
+total_sales,
+SUM(total_sales) OVER() overall_sales,
+CONCAT(ROUND((CAST (total_sales AS FLOAT) / SUM(total_sales) OVER())*100, 2), '%') AS percentage_of_total
+FROM sub_category_sales
+ORDER BY total_sales DESC
